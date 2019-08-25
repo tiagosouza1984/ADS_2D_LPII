0da25dedcceeb3644ac2c5a79a432e89e81d37b9
MAC <mac@MacBook-Alvosoft-Tiago.local>
2019-08-24 21:23:13 -0300

    update aula01 e aula01_test

--- aula01.py
+++ aula01.py
@@ -1,14 +1,22 @@
+
+
 def ehPrimo(num):
     '''
     Função que retorna true se é primo e false caso contrário
+    ps: número 2 não entra no "for", ele já é considerado
+    primo
     '''
-    for i in range(2, num // 2):
+    for i in range(2, num):
         if num % i == 0:
             return False
     return True
 
 
 def ListaPrimos2(num):
+    '''
+    Função que retorna lista de primos até o número passado
+    no parâmetro da função
+    '''
     primos = []
     for i in range(2, num):
         if ehPrimo(i):
@@ -23,10 +31,11 @@ def listaPrimos(num):
         for j in range(2, i):
             if i % j == 0:
                 primo = False
+
         if primo:
             primos.append(i)
     return primos
 
 
 if __name__ == "__main__":
-    print(listaPrimos(15))
+    print(ListaPrimos2(5))
--- aula01_test.py
+++ aula01_test.py
@@ -1,6 +1,6 @@
 import aula01 as a1
 
 
-def test_eh_primo_8():
-    primos = a1.listaPrimos(8)
-    assert len(primos) == 4
+def test_eh_primo_7():
+    primos = a1.ListaPrimos2(7)
+    assert len(primos) == 3
