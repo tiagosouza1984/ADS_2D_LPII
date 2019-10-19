112c4e10d72c7406516aa042f1e35eb2e2448666
MAC <mac@MacBook-Alvosoft-Tiago.local>
2019-10-19 20:10:08 -0300

    test_app atualizado

--- com/tiago/test/test_app_web_start.py
+++ com/tiago/test/test_app_web_start.py
@@ -10,4 +10,4 @@ def test_root_status():
 def test_root_url():
     instancia_app = app_web_start.app.test_client()
     response = instancia_app.get('/')
-    assert response.data.decode('utf-8') == "Index Page", "Deveria existir "
+    assert response.data.decode('utf-8') == "Index Page!", "Deveria existir "
